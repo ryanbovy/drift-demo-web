b2ad0cf5d55fcf262d11765ad228801050bc61a9
Joshua Perk <>
2022-04-13 21:24:12 -0400

    Removed loadDrift step when watching changes to widget and playbook

--- pages/index.vue
+++ pages/index.vue
@@ -255,13 +255,9 @@ export default {
   },
   watch: {
     widgetId (newId, oldId) {
-      console.log('widget change')
-      this.loadDrift()
       this.firePlaybook()
     },
     playbookName (newPlaybook, oldPlaybook) {
-      console.log('playbook change')
-      this.loadDrift()
       this.firePlaybook()
     }
   },
@@ -274,7 +270,6 @@ export default {
     // This is where we'll put all our functions to make our code organized
     loadDrift () {
       // A function to run the standard install code. The widgetId variable can be set (otherwise uses a default value)
-      // Drift's install code doesn't cooperate well with our linter, so turning that off here
       /*eslint-disable */
       'use strict'
       !(function () {
@@ -282,19 +277,19 @@ export default {
         if (!t.init) {
           if (t.invoked) { return void (window.console && console.error && console.error('Drift snippet included twice.')) }
           t.invoked = !0, t.methods = ['identify', 'config', 'track', 'reset', 'debug', 'show', 'ping', 'page', 'hide', 'off', 'on'],
-            t.factory = function (e) {
-              return function () {
-                const n = Array.prototype.slice.call(arguments)
-                return n.unshift(e), t.push(n), t
-              }
-            }, t.methods.forEach(function (e) {
-              t[e] = t.factory(e)
-            }), t.load = function (t) {
-              const e = 3e5; const n = Math.ceil(new Date() / e) * e; const o = document.createElement('script')
-              o.type = 'text/javascript', o.async = !0, o.crossorigin = 'anonymous', o.src = 'https://js.driftt.com/include/' + n + '/' + t + '.js'
-              const i = document.getElementsByTagName('script')[0]
-              i.parentNode.insertBefore(o, i)
+          t.factory = function (e) {
+            return function () {
+              const n = Array.prototype.slice.call(arguments)
+              return n.unshift(e), t.push(n), t
             }
+          }, t.methods.forEach(function (e) {
+            t[e] = t.factory(e)
+          }), t.load = function (t) {
+            const e = 3e5; const n = Math.ceil(new Date() / e) * e; const o = document.createElement('script')
+            o.type = 'text/javascript', o.async = !0, o.crossorigin = 'anonymous', o.src = 'https://js.driftt.com/include/' + n + '/' + t + '.js'
+            const i = document.getElementsByTagName('script')[0]
+            i.parentNode.insertBefore(o, i)
+          }
         }
       }())
       drift.SNIPPET_VERSION = '0.3.1'
@@ -310,9 +305,6 @@ export default {
     },
     firePlaybook () {
       // A function to fire the desired playbook
-      // Drift's install code doesn't cooperate well with our linter, so turning that off here
-      /*eslint-disable */
-
       // Choose selected playbook
       switch (this.playbookName) {
         case 'Fastlane':
@@ -350,7 +342,8 @@ export default {
           }
       }
       // Fire selected playbook
-      drift.on('ready', function (api, payload) {
+      /*eslint-disable */
+      drift.on('ready', (api, payload) => {
         console.log('drift ready');
         drift.api.startInteraction({
           interactionId: this.playbookId,
