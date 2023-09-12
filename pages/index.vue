2fd817fb99b7941f783b782c1d9be74e594ef19d
Ryan Bovy <r.bovy16@gmail.com>
2023-09-12 14:10:10 -0600

    remove Lift AI

--- pages/index.vue
+++ pages/index.vue
@@ -15,21 +15,6 @@
     <!-- SIDEBAR MENU -->
     <TheSideBar />
     <!-- END SIDEBAR MENU -->
-    <!--BEGIN  MarketLinc Monitor. -->
-    <script type="text/javascript">
-      try {
-      var ml_url =
-      'https://lift-ai-js.marketlinc.com/v2.drift-demo.com/deployment.js'
-      var ml_script = document.createElement('script')
-      ml_script.type = 'text/javascript'
-      ml_script.id = 'ml_snippet_script_id'
-      ml_script.src = ml_url + '?' + Math.floor(Math.random() * 1000000000)
-      document.getElementsByTagName('head')[0].appendChild(ml_script)
-      } catch (e) {
-      /*the endpoint is not available, do nothing*/
-      }
-    </script>
-    <!-- END MarketLinc Monitor. -->
   </main>
 </template>
 
