a2d02f3c357ec7f2615fb1b1b6dde135ff3f1bb7
Ryan Bovy <34945910+ryanbovy@users.noreply.github.com>
2024-10-24 15:18:23 -0600

    Remove driftrace

--- pages/index.vue
+++ pages/index.vue
@@ -534,16 +534,17 @@ export default {
             goToConversation: false,
             replaceActiveConversation: true
           })
-        } else {
+        } /*
+        else {
           /* Weird fluke in Drift where the widget is already loaded and the only way to
           load an ABM bot is through alternative targeting (not interactionId). Therefore,
           we have to set a cookie, then wait a MILLIsecond and refresh the "page"
-          */
           setTimeout(() => {
             window.history.replaceState(null, null, '#driftRace')
             drift.page()
           }, 500)
         }
+        */
       })
     },
     setCookie (name, value, days) {
